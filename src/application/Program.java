9948986499dc96b0ac600ab3efe9102516ba38b6
Felipe Augusto <felipealcmagalhaes@gmail.com>
2021-07-12 10:40:24 -0300

    initial projects

---
+++ src/application/Program.java
@@ -0,0 +1,82 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> list = new ArrayList<>(); 
+		
+		System.out.print("How many employees will be registered ? ");
+		int n = sc.nextInt();
+		sc.nextLine();
+		
+		for(int i = 1; i <= n; i++) {
+			
+			System.out.println();
+			System.out.println("Employee #" + i);
+			
+			System.out.print("Id: ");
+			Integer id = sc.nextInt();
+			
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			
+			System.out.print("Salary: ");
+			Double salary = sc.nextDouble();
+			
+			list.add(new Employee(id, name, salary));
+			/*OU 
+			 * Employee emp = new Employee(id, name, salary);
+			 * list.add(emp); 
+			 * */
+		}
+		
+		System.out.println();
+		System.out.print("Enter the employee id that will have salary increase: ");
+		int id = sc.nextInt();
+		Employee emp = list.stream().filter(x -> x.getId() == id ).findFirst().orElse(null);
+		/* USANDO A FORMA MANUAL SUBSTITUA A LINHA A CIMA POR:
+		 * INTEGER POS = POSITION(LIST, ID);
+		 * E O EMP DO IF ABAIXO POR 'POS'*/
+		if(emp == null) {
+			System.out.println("This id does not exist! ");
+		}else {
+			
+			System.out.print("Enter the percentage: ");
+			double percentage = sc.nextDouble();
+			emp.increaseSalary(percentage);
+		}
+		
+		System.out.println();
+		System.out.println("List of employees:");
+		for(Employee obj : list) {
+			System.out.println(obj);
+		}
+		
+		sc.close();
+	}
+	
+	//FORMA DE ENCONTRAR A POSIÇÃO DO ID MANUALMENTE
+	public static Integer position(List<Employee> list, int id) {
+		for (int i = 0; i < list.size(); i++) {
+			if(list.get(i).getId() == id) {
+				return i;
+			}
+		}
+		return null;
+	}
+
+}
